8543c4c01329e6c075bb4342d01bf10e8fb01e7b
0xkjAlbert <1046491850@qq.com>
2020-07-22 16:14:54 +0800

    Delete some code

--- getCode.py
+++ getCode.py
@@ -3,7 +3,6 @@ from lxml import etree
 
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"}
 URL = str(input("Please input url:"))
-#rsp = requests.get(url = "https://blog.csdn.net/u013733326/article/details/79847918", headers = headers)
 rsp = requests.get(url = URL, headers = headers)
 for i in range(1,60):
     try:
@@ -11,10 +10,6 @@ for i in range(1,60):
         a = etree.HTML(rsp.text)
         info = a.xpath(xpath1)
         print(info[0].xpath('string(.)'))
-     #   while True:
-     #       enter = str(input('Press enter to next:'))
-     #       if enter == '\n':
-     #           break;
         while input('Press any key to continue:'):
             break
     except IndexError:
