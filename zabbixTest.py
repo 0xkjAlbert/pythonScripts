4e02ed83959170a49d95a1cf45b24bd8130cf61b
0xkjAlbert <1046491850@qq.com>
2020-07-23 23:53:29 +0800

    Add zabbixTest.py

---
+++ zabbixTest.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+apiURL="http://192.168.39.220/zabbix/api_jsonrpc.php"
+apiHeader={"Content-Type":"application/json"}
+
+def loginZabbix(URL,Header):
+    global zabbixToken
+    logData = json.dumps({
+        "jsonrpc": "2.0",
+        "method": "user.login",
+        "params": {
+            "user": "centos",
+            "password": "zabbix"
+        },
+        "id": 1,
+        })
+    token = requests.post(url = URL, data = logData, headers = Header)
+    if 'error' in token.text:
+        return "error"
+    zabbixToken =  token.json()["result"]
+    return zabbixToken
+
+def getHostgroup(URL = apiURL, Header = apiHeader):
+    getData = json.dumps({
+        "jsonrpc":"2.0",
+        "method":"hostgroup.get",
+        "params":{
+        "output": "extend",
+        "filter": {
+            "name": [
+                "39 group",
+                ]
+            }
+        },
+        "auth":loginZabbix(URL, Header),
+        "id":1,
+    })
+    print(getData)
+    result = requests.post(url = URL, headers = Header, data = getData)
+    print(result.text)    
+
+getHostgroup()
